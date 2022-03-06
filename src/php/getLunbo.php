abf30634480969c2822ad8fda9f0e8ef00e58fe7
xiaoxiong <815184693@qq.com>
2022-03-06 22:01:35 +0800

    hah

---
+++ src/php/getLunbo.php
@@ -0,0 +1,29 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$data = [
+    "status"=> 0,
+    "message"=>[
+        [
+            "url"=>"https://www.baidu.com/",
+            "img"=>"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F121420113514%2F201214113514-1-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1648882990&t=3d9cda3fa51cc7017d8ab203413cde8d"
+        ],
+        [
+            "url"=>"https://www.bilibili.com/",
+            "img"=>"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1113%2F032120114622%2F200321114622-4-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1648883114&t=138e5c5c92a45315586325e7d7c5e6d5"
+        ],
+        [
+            "url"=>"https://www.github.com/",
+            "img"=>"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1113%2F041620104229%2F200416104229-8-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1648883114&t=8ed0a7d957fe6bfc402a7bbff03f32cc"
+        ],
+        [
+            "url"=>"https://www.huya.com/",
+            "img"=>"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1113%2F060120105F7%2F200601105F7-1-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1648883114&t=9cd41b402a563c54102bd2a74a07dbe8"
+        ]
+    ]
+];
+echo json_encode($data);
+?>
+   
+
+
+
